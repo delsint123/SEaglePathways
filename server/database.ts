342d4443be4d82e06e931eca715726279aad38ca
Delphine Sintamour <delsint123@gmail.com>
2023-10-18 07:40:40 -0400

    Update env variables in database.ts

--- server/database.ts
+++ server/database.ts
@@ -2,20 +2,12 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
  
-// const db = mysql
-//     .createPool({
-//         host: process.env.MYSQL_HOST,
-//         user: process.env.MYSQL_USER,
-//         password: process.env.MYSQL_PASSWORD,
-//         database: process.env.MYSQL_DATABASE,
-//     }).promise()
-
 const db = mysql
     .createPool({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'SEaglePathways',
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
     }).promise()
 
 export default db;
